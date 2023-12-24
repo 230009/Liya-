fab06929900b89da2585e693ea269ab388c4fc60
Лия Жарылқасын <liazarylkasyn@MacBook-Air-Lia.local>
2023-12-25 00:42:40 +0600

    first commit

---
+++ src/models/Point.java
@@ -0,0 +1,18 @@
+package models;
+public class Point {
+    double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
